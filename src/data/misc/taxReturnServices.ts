1f04485b807ab9aeef4263cf4766c847f6bf3d91
Piyush Paliwal <ppaliwal.89@gmail.com>
2023-01-02 11:27:24 +0530

    Tax Page completion

---
+++ src/data/misc/taxReturnServices.ts
@@ -0,0 +1,38 @@
+// used in the services-4 block
+export const taxReturnServices = [
+  {
+    id: 1,
+    linkUrl: '#',
+    title: 'Documentation',
+    icon: 'uil-dollar-sign',
+    description: `Arrangement of source documents in digital file cabinet / portal`
+  },
+  {
+    id: 2,
+    linkUrl: '#',
+    title: 'Calculations',
+    icon: 'uil-focus-target',
+    description: `Prepare tax calculation worksheets`
+  },
+  {
+    id: 3,
+    linkUrl: '#',
+    title: 'Data Entry',
+    icon: 'uil-abacus',
+    description: `Entry or Import of tax data into taxation softwares`
+  },
+  {
+    id: 4,
+    linkUrl: '#',
+    title: 'Drafting',
+    icon: 'uil-user-exclamation',
+    description: `Preparation of draft tax returns`
+  },
+  {
+    id: 5,
+    linkUrl: '#',
+    title: 'Review',
+    icon: 'uil-user-exclamation',
+    description: `Review of documentation along with you`
+  }
+]
