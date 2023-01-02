0bb3929a74e9af7ac31bd578063a464995268a57
Piyush Paliwal <ppaliwal.89@gmail.com>
2023-01-02 12:06:57 +0530

    Included Invoice Processing services

---
+++ src/data/misc/invoiceServiceList.ts
@@ -0,0 +1,7 @@
+export const invoiceServiceList = [
+  ['Data Validation', 'Matching POs to Invoices'],
+  ['Examine Cash flow', 'Obtain information from line items and headers'],
+  ['Making electronic invoices & receipts', 'Log transactions'],
+  ['Scan paper invoices', 'Generate PO receipts'],
+  ['Assistance by Chat, Email and Phone', 'Dashboard monitoring for approval']
+]
