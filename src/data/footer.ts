d9cb6862d6f266c4ffeed07f6ec345f95cce86fd
Piyush Paliwal <ppaliwal.89@gmail.com>
2023-01-19 00:03:36 +0800

    Updated policy links on the website

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -2,8 +2,8 @@ const footerNav = [
   { title: 'About Us', url: '/about-us' },
   { title: 'FAQs', url: '/faq' },
   // { title: 'Life@Ambit', url: '/life' },
-  { title: 'Contact', url: '/contact' }
-  // { title: 'Privacy Policy', url: '/privacy-policy' }
+  { title: 'Contact', url: '/contact' },
+  { title: 'Privacy Policy', url: '/privacy-policy' }
 ]
 
 export default footerNav
