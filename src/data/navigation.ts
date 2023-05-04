b7c44318daa48a06ed31a0230bbd0e44b9eef697
piyushpaliwal <ppaliwal.89@gmail.com>
2023-05-04 11:37:43 +0530

    added tax page

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -7,14 +7,22 @@ const services = [
     title: 'Bookkeeping',
     children: [
       { id: 111, title: 'Accounting & Bookkeeping', url: '/services/accounting-bookkeeping' },
-      { id: 111, title: 'Catch-up Bookkeeping', url: '/services/catch-up-bookkeeping' }
+      { id: 112, title: 'Catch-up Bookkeeping', url: '/services/catch-up-bookkeeping' },
+      { id: 113, title: 'Payroll', url: '/services/payroll' },
+      { id: 114, title: 'Accounts Receivable', url: '/services/accounts-receivable' },
+      { id: 115, title: 'Accounts Payable', url: '/services/accounts-payable' },
+      { id: 116, url: '/services/invoice-processing', title: 'Invoice Processing' }
     ]
   },
-  { id: 2, url: '/services/payroll', title: 'Payroll', children: [] },
-  { id: 3, url: '/services/accounts-receivable', title: 'Accounts Receivable', children: [] },
-  { id: 4, url: '/services/accounts-payable', title: 'Accounts Payable', children: [] },
-  { id: 5, url: '/services/tax-return', title: 'Tax Return Preparation', children: [] },
-  { id: 6, url: '/services/invoice-processing', title: 'Invoice Processing', children: [] },
+  {
+    id: 5,
+    url: '#',
+    title: 'Tax',
+    children: [
+      { id: 51, url: '/services/tax-service', title: 'Tax Service' },
+      { id: 52, url: '/services/tax-return', title: 'Tax Return Preparation' }
+    ]
+  },
   { id: 7, url: '/services/virtual-assistance', title: 'Virtual Assistance', children: [] },
   { id: 8, url: '/services/ertc', title: 'ERTC', children: [] },
   { id: 9, url: '/services/financial-reporting', title: 'Financial Reporting', children: [] }
