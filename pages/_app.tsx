21664c2a5925ee67351693a93b63fa1fc06a0976
Piyush Paliwal <ppaliwal.89@gmail.com>
2023-07-26 11:56:35 +0530

    updated google tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -82,12 +82,40 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Ambit offers outsourced bookkeeping and accounting services for CPA and accounting firms in the USA. Hire highly qualified and dedicated offshore bookkeepers"
         />
       </Head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11267136386"></Script>
       <Script id="google-tag-manager" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NCM2SWG');`}
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config','AW-11267136386');
+        `}
+      </Script>
+      {/* Facebook Pixel Code */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1664746134033631');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          alt="facebook-pixel"
+          height="1"
+          width="1"
+          src="https://www.facebook.com/tr?id=1664746134033631&ev=PageView&noscript=1"
+        />
+      </noscript>
+      <Script id="google-event">
+        {`gtag('event', 'conversion', {'send_to': 'AW-11267136386/6v1FCPSZp8IYEIK3y_wp'});`}
       </Script>
       <Component {...pageProps} />
     </Fragment>
