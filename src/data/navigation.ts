3cfe999c33544e65f0a340fecd63381d07c4a7fc
nabiluddin <nabiluddin@winfotech.co>
2024-11-26 10:02:06 +0000

    corrected navigations for services

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -4,50 +4,50 @@ const services = [
   {
     id: 1,
     url: '#',
-    title: 'Accounting',
-    children: [
-      { id: 111, title: 'Accounting & Bookkeeping', url: '/services/accounting-bookkeeping' },
-      { id: 112, title: 'Catch-up Bookkeeping', url: '/services/catch-up-bookkeeping' },
-      { id: 113, title: 'Payroll', url: '/services/payroll' },
-      { id: 114, title: 'Accounts Receivable', url: '/services/accounts-receivable' },
-      { id: 115, title: 'Accounts Payable', url: '/services/accounts-payable' },
-      { id: 116, url: '/services/invoice-processing', title: 'Invoice Processing' }
-    ]
-  },
-  {
-    id: 5,
-    url: '#',
-    title: 'Tax',
-    children: [
-      { id: 51, url: '/services/tax-service', title: 'Tax Planning & Advisory' },
-      { id: 52, url: '/services/tax-return', title: 'Tax Return Preparation' },
-    ]
-  },
-  { id: 9, url: '/services/financial-reporting', title: 'Financial Reporting', children: [] },
-  { id: 10, url: '/services/dedicated-staffing', title: 'Dedicated Staffing', children: [] },
-  { id: 11, url: '/remote-accounting-tax-solutions', title: 'Remote Staffing', children: [] },
-  {
-    id: 6,
-    url: '#',
     title: 'For CPAs',
     children: [
       {
-        id: 61,
+        id: 11,
         url: '#',
-        title: 'For Businesses',
+        title: 'Accounting',
         children: [
-          { id: 611, url: '/services/bookkeeping-and-accounting', title: 'Bookkeeping And Accounting' },
-          { id: 612, url: '/services/tax-services', title: 'Tax Services' },
+          { id: 111, title: 'Accounting & Bookkeeping', url: '/services/accounting-bookkeeping' },
+          { id: 112, title: 'Catch-up Bookkeeping', url: '/services/catch-up-bookkeeping' },
+          { id: 113, title: 'Payroll', url: '/services/payroll' },
+          { id: 114, title: 'Accounts Receivable', url: '/services/accounts-receivable' },
+          { id: 115, title: 'Accounts Payable', url: '/services/accounts-payable' },
+          { id: 116, url: '/services/invoice-processing', title: 'Invoice Processing' }
         ]
-      }
+      },
+      {
+        id: 12,
+        url: '#',
+        title: 'Tax',
+        children: [
+          { id: 121, url: '/services/tax-service', title: 'Tax Planning & Advisory' },
+          { id: 122, url: '/services/tax-return', title: 'Tax Return Preparation' },
+        ]
+      },
+      { id: 13, url: '/services/financial-reporting', title: 'Financial Reporting', children: [] },
+      { id: 14, url: '/services/dedicated-staffing', title: 'Dedicated Staffing', children: [] },
+      { id: 15, url: '/remote-accounting-tax-solutions', title: 'Remote Staffing', children: [] }
     ]
   },
+  {
+    id: 2,
+    url: '#',
+    title: 'For Businesses',
+    children: [
+      { id: 21, url: '/services/bookkeeping-and-accounting', title: 'Bookkeeping And Accounting' },
+      { id: 22, url: '/services/tax-services', title: 'Tax Services' },
+    ]
+  }
 ]
 
 const company = [
   { id: 1, url: '/about-us', title: 'About Us', children: [] },
   { id: 2, url: '/faq', title: 'FAQ', children: [] },
-  { id: 7, url: '/privacy-policy', title: 'Privacy Policy', children: [] }
+  { id: 3, url: '/privacy-policy', title: 'Privacy Policy', children: [] }
 ]
 
 export { home, services, company }
